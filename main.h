b1daa278ed98772efbb813ef0f63bc8f93101a44
Fasakin Henry <fasakinhenry@gmail.com>
2023-12-02 23:11:26 +0100

    Feat: Add standard header files in main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <limits.h>
+
+#endif
